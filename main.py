eb8aad3c397d30e60c6c2c24ae110351e503b956
Bear KF <amberkesslerfreer@gmail.com>
2023-03-02 20:01:33 -0600

    Finetune xfer functions

--- main.py
+++ main.py
@@ -22,5 +22,6 @@ user_two = {
 
 import venmo_clone_functions
 
-# venmo_clone_functions.authenticate_login(user_one)
+venmo_clone_functions.authenticate_login(user_one)
 venmo_clone_functions.show_banks(user_one)
+venmo_clone_functions.confirm_xfer(user_one, user_two)
--- venmo_clone_functions.py
+++ venmo_clone_functions.py
@@ -6,7 +6,7 @@ def authenticate_login(user):
         password = input("Type your password: ")
 
         if password == ((user)['password']) and username == ((user)['username']):
-            print(f"Login Successful! Welcome, " + f"{user}!" )
+            print(f"Login Successful! Welcome, " + f"{(user)['full_name']}!" )
             valid_login = True
         else:
             print("Login failed. Try again.")
@@ -21,11 +21,17 @@ def show_banks(user):
 def select_bank():
     pass
 
-def check_account_bal(user):
-    xfer_amount = int[input("How much would you like to transfer? Please enter a whole dollar amount. ")]
-    while xfer_amount > ((user)['account_balance']):
-        print(f"You don't have enough in your account. Your account balance is " + f'{(user)['account_balance']}.') # Bonus code: + f"Please choose a connected bank to fund your account first.")
+def check_account_bal(user1,user2):
+    xfer_amount = input("How much would you like to transfer? Please enter a whole dollar amount. ")
+    amount_to_check = int(xfer_amount)
+    if amount_to_check > ((user1)['account_balance']):
+        print(f"You don't have enough in your account. Your account balance is " + f"{(user1)['account_balance']}.") # Bonus code: + f"Please choose a connected bank to fund your account first.")
         # Bonus code: select_bank(user)
+    else:
+        user1['account_balance'] = (((user1)['account_balance']) - amount_to_check)
+        user2['account_balance'] = (((user2)['account_balance']) + amount_to_check)
+        print(f"Your transfer was successful! Your account balance is now " + f"{(user1)['account_balance']}.")
+        complete_xfer(user1, user2)
 
 ###
 # Ask user_one if they would like to transfer money to user_two.
@@ -39,12 +45,19 @@ def check_account_bal(user):
 # If not, print the final account balance of user_one and exit.
 
 def confirm_xfer(user1, user2):
-    start_xfer = input(f"Would you like to make a transfer to " + f'{user2}' + f"? (Y/N)")
+    start_xfer = input(f"Would you like to make a transfer to " + f"{(user2)['full_name']}" + f"? (Y/N)")
     if start_xfer == 'Y':
-        check_account_bal(user1)
+        check_account_bal(user1, user2)
     else:
         print("Goodbye!")
-
+    
+def complete_xfer(user1, user2):
+    end_xfer = input(f"Would you like to make another transfer to " + f"{(user2)['full_name']}" + f"? (Y/N)")
+    if end_xfer == 'Y':
+        check_account_bal(user1, user2)
+    else:
+        print("Your final balance is " + f"{(user1)['account_balance']}.")
+        print("Goodbye!")
 
 def update_account_balances():
     pass
